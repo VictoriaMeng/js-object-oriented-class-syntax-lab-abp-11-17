3aff29e12406553480777d5577bd8d1037399385
Victoria Meng <nights1ststar@protonmail.com>
2017-11-12 01:21:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-class BoardMember = function (name, homeState, training) {
+class BoardMember = {
+  constructor(name, homestate, training)
   this.name = name;
   this.homeState = homeState;
   this.training = training;
